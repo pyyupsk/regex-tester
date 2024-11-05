72f8fd7907608c1b233938830e70aa6204501499
pyyupsk <pyyupsk@proton.me>
2024-11-05 19:58:50 +0700

    feat(ui): install shadcn/ui and update styles 🎨
    Added new components and modified layout, styles, and utils.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
 
+import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import { GeistMono } from "geist/font/mono";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -13,8 +15,10 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={cn(GeistSans.variable, GeistMono.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
